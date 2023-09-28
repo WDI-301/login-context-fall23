071331ab3c9fbe0cd94bda041a1cf788bf866fde
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-09-28 09:18:06 -0700

    setup conditional render for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,7 +11,6 @@ const [value, setValue] = useState({
   password: ''
 })
 
-
 const {theme} = useContext(ThemeContext)
 
 const login = useContext(LoginContext)
@@ -27,39 +26,50 @@ const onChangeHandler = event => {
 
   return (
     <div id='login' className={theme}>
-{/* 
-    Username: {login.username} 
-    <br/> */}
-    {/* Password: {login.password} */}
-    {/* <br /> */}
-    <label htmlFor='username'>Username: </label>
-    <input 
-      id='username'
-      type='text'
-      name='username'
-      value={value.username}
-      onChange={onChangeHandler}
-    />
-    <br />
-    <label htmlFor='password'>Password: </label>
-    <input
-      id='password' 
-      type='text'
-      name='password'
-      value={value.password}
-      onChange={onChangeHandler}
-    />
-    <br />
-    {/* <button onClick={() => dispatch({
-      type: "submit",
-      payload: value
-    })}>Submit</button> */}
-    {/* <button onClick={() => submitLogin(dispatch, value)}>Submit</button> */}
-    <button onClick={() => fetchLogin(dispatch, value)}>Login</button>
-    <button onClick={() => registerUser(dispatch, value)}>Register</button>
-    {/* // Welcome {Username}
-    // Message: {Username} Successfully Registered */}
+      {
+        login.username === '' ?
+        <>
+          {/* 
+          Username: {login.username} 
+          <br/> */}
+          {/* Password: {login.password} */}
+          {/* <br /> */}
+          <label htmlFor='username'>Username: </label>
+          <input 
+            id='username'
+            type='text'
+            name='username'
+            value={value.username}
+            onChange={onChangeHandler}
+          />
+          <br />
+          <label htmlFor='password'>Password: </label>
+          <input
+            id='password' 
+            type='text'
+            name='password'
+            value={value.password}
+            onChange={onChangeHandler}
+          />
+          <br />
+          {/* <button onClick={() => dispatch({
+            type: "submit",
+            payload: value
+          })}>Submit</button> */}
+          {/* <button onClick={() => submitLogin(dispatch, value)}>Submit</button> */}
+          <button onClick={() => fetchLogin(dispatch, value)}>Login</button>
+          <button onClick={() => registerUser(dispatch, value)}>Register</button>
+          {/* // Welcome {Username}
+          // Message: {Username} Successfully Registered */}
+        </>
+        : 
+        <>
+          <button>Logout</button>
+        </>
+      }
     </div>
+
+    
 
   )
 }
